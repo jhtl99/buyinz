ffa3fbf932ee0f5998b1a924de3d7470e922b7cb
Jayden Lin <jaydenl@andrew.cmu.edu>
2026-02-13 12:32:00 -0500

    analytics again

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -14,12 +14,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-          <Analytics />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
